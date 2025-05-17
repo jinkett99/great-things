f25de0c7de25054af3f34fac5a80af410dc59b23
Monkeyyboy <jinkett99@gmail.com>
2025-05-17 19:32:39 +0800

    RAG-webscraper - research_assistantv2

---
+++ .ipynb_checkpoints/README-checkpoint.md
@@ -0,0 +1,3 @@
+# LangChain
+1. This repository contains various PoC projects which utilizes LangChain to develop applications powered by LLMs. 
+2. We aim to refine our development codes and move iteratively into production and deployment cycles with more advanced libraries like LangSmith and LangGraph.
---
+++ RAG-webscraper/research_assistantv2.py
@@ -0,0 +1,100 @@
+# import dependencies
+import streamlit as st
+from langchain_community.document_loaders import SeleniumURLLoader, PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_chroma import Chroma
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_ollama.llms import OllamaLLM
+import nltk
+
+# download tools from NLTK library
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+
+# Prompt template for LLM
+template = """
+    You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. \
+    If you don't know the answer, say that you don't know. Use three sentences maximum and keep the answer concise.
+    Question: {question}
+    Context: {context}
+    Answer: 
+"""
+
+# define embeddings model for vector store
+model_name = "hkunlp/instructor-large"
+model_kwargs = {'device': 'cpu'}
+encode_kwargs = {'normalize_embeddings': True}
+hf = HuggingFaceEmbeddings(
+    model_name=model_name,
+    model_kwargs=model_kwargs,
+    encode_kwargs=encode_kwargs
+)
+
+# Instantiate LLM
+model = OllamaLLM(model="llama3.2")
+
+# load webpage contents
+def load_page(url): 
+    loader = SeleniumURLLoader(
+        urls=[url],
+        headless=False #run in non-headless mode for Cloudflare to see a real browser
+    )
+    documents = loader.load()
+    return documents
+
+# load pdf contents
+def load_pdf(uploaded_file): 
+    with open("docs/temp.pdf", "wb") as f:
+        f.write(uploaded_file.read())
+    return PyPDFLoader("docs/temp.pdf").load()
+
+# Split text into smaller documents - that can fit better to LLM context window, for loading into vector database/in-memory vector store. 
+def split_text(documents):
+    if not documents: 
+        return []
+    text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000,
+        chunk_overlap=200, 
+        add_start_index=True
+    )
+    data = text_splitter.split_documents(documents)
+    return data
+
+# Retrieve docs - Based on user query.
+def retrieve_docs(query, vector_store):
+    retriever = vector_store.as_retriever(search_type="similarity", search_kwargs={"k": 12})
+    retrieved_docs = retriever.invoke(query)
+    return retrieved_docs
+
+# pass context-rich combined prompt to LLM to generate fontext-aware response.
+def answer_question(question, context): 
+    prompt = ChatPromptTemplate.from_template(template)
+    chain = prompt | model
+    return chain.invoke({"question": question, "context": context})
+
+# Streamlit config? 
+st.title("Research Assistant")
+url = st.text_input("Enter URL:")
+upload = st.file_uploader("Upload a PDF file", type=["pdf"])
+documents = []
+
+# load content type - select one.
+if url: 
+    documents = load_page(url)
+elif upload: 
+    documents = load_pdf(upload)
+
+# perform pipeline steps
+if documents: 
+    chunked_docs = split_text(documents)
+    vector_store = Chroma.from_documents(documents=chunked_docs, embedding=hf, persist_directory="chroma_db")
+
+# use query question to retrieve docs
+question = st.chat_input()
+if question: 
+    st.chat_message("user").write(question) #display user's question.
+    retrieve_documents = retrieve_docs(question, vector_store)
+    context = "\n\n".join(doc.page_content for doc in retrieve_documents)
+    answer = answer_question(question, context)
+    st.chat_message("assistant").write(answer) #display answer.
